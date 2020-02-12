84d0c2ee8189ec4ff49b8282ad83502bdc658d8f
Ramaxa911 <roma9pm@i.ua>
2020-02-12 13:44:26 +0000

    fdgdg

--- js/index.js
+++ js/index.js
@@ -1,8 +1,27 @@
-const display = document.querySelector('.display');
+const display = document.querySelector(".display");
 
-document.querySelectorAll('.digits button')
-    .forEach( button => button.addEventListener('click', digitPressed));
+document.querySelectorAll(".digits button")
+    .forEach( button => button.addEventListener("click", digitPressed));
 
 function digitPressed(ev) {
     display.value += ev.target.innerText;
+}
+
+document.querySelectorAll(".opers button")
+    .forEach( button => button.addEventListener("click", opersPressed));
+
+function opersPressed(ev) {
+    display.value += ev.target.innerText;
+}
+
+document.querySelector(".eq").addEventListener("click", calculete);
+
+function calculete() {
+    display.value = eval(display.value);
+}
+
+document.querySelector(".clear").addEventListener("click", clear);
+
+function clear() {
+    display.value = "";
 }
